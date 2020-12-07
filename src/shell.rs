5e8ca9dffc73fe084db94c0fe3700087e949fa47
Takuya Hashimoto <thash@amazon.com>
2020-12-08 01:37:42 +0900

    replace `0 < input.len()` to `!input.is_empty()`
    https://rust-lang.github.io/rust-clippy/master/index.html#len_zero

--- src/shell.rs
+++ src/shell.rs
@@ -99,7 +99,7 @@ impl<'a> ShellReader<'a> {
 fn parse(line: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut ret = vec![];
     let mut input = line.trim_start();
-    while 0 < input.len() {
+    while !input.is_empty() {
         if input.starts_with("'") {
             let mut tok = String::new();
             let mut iter = input.chars();
