1c27f9c672203c19ebface0648f2e04579028aaa
Ryota Sakamoto <skmt@amazon.com>
2023-06-23 23:21:45 +0900

    test: split integration test into each command

---
+++ tests/admin.rs
@@ -0,0 +1,100 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pub mod util;
+
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+
+#[tokio::test]
+async fn test_create_table() -> Result<(), Box<dyn std::error::Error>> {
+    let table_name = "table--test_create_table";
+
+    // $ dy admin create table <table_name> --keys pk
+    let mut c = util::setup().await?;
+    let create_cmd = c.args(&[
+        "--region", "local", "admin", "create", "table", table_name, "--keys", "pk",
+    ]);
+    create_cmd
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "name: {}\nregion: local",
+            &table_name
+        )));
+
+    // $ dy admin desc <table_name>
+    let mut c = util::setup().await?;
+    let desc_cmd = c.args(&["--region", "local", "desc", table_name]);
+    desc_cmd
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "name: {}\nregion: local",
+            &table_name
+        )));
+
+    util::cleanup(vec![table_name]).await
+}
+
+#[tokio::test]
+async fn test_create_table_with_region_local_and_port_number_options(
+) -> Result<(), Box<dyn std::error::Error>> {
+    let port = 8001;
+    let table_name = "table--test_create_table_with_region_local_and_port_number_options";
+
+    // $ dy admin create table <table_name> --keys pk
+    let mut c = util::setup_with_port(port).await?;
+    let create_cmd = c.args(&[
+        "--region",
+        "local",
+        "--port",
+        &format!("{}", port),
+        "admin",
+        "create",
+        "table",
+        table_name,
+        "--keys",
+        "pk",
+    ]);
+    create_cmd
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "name: {}\nregion: local",
+            &table_name
+        )));
+
+    // $ dy admin desc <table_name>
+    let mut c = util::setup_with_port(port).await?;
+    let desc_cmd = c.args(&[
+        "--region",
+        "local",
+        "--port",
+        &format!("{}", port),
+        "desc",
+        table_name,
+    ]);
+    desc_cmd
+        .assert()
+        .success()
+        .stdout(predicate::str::contains(format!(
+            "name: {}\nregion: local",
+            &table_name
+        )));
+
+    util::cleanup_with_port(vec![table_name], port).await
+}
---
+++ tests/auxiliary.rs
@@ -0,0 +1,57 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+pub mod util;
+
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+                           // use assert_cmd::cmd::Command; // Run programs - it seems to be equal to "use assert_cmd::prelude::* + use std::process::Command"
+
+#[tokio::test]
+async fn test_help() -> Result<(), Box<dyn std::error::Error>> {
+    let mut dynein_cmd = Command::cargo_bin("dy")?;
+    let cmd = dynein_cmd.arg("--help");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("dynein is a command line tool"));
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_custom_config_location() -> Result<(), Box<dyn std::error::Error>> {
+    use std::path::Path;
+
+    let dummy_dir = "./tests/dummy_dir";
+    let config_dir = dummy_dir.to_string() + "/.dynein";
+
+    // cleanup config folder in case it was already there
+    util::cleanup_config(dummy_dir).await.ok();
+
+    // dy config clear
+    Command::cargo_bin("dy")?
+        .args(&["config", "clear"])
+        .env("DYNEIN_CONFIG_DIR", dummy_dir)
+        .output()?;
+
+    // check config folder created at our desired location
+    assert!(Path::new(&config_dir).exists());
+
+    // cleanup config folder
+    util::cleanup_config(dummy_dir).await.ok();
+
+    Ok(())
+}
